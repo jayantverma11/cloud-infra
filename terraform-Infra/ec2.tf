6b8ac9d1a6d828a2b21950d7b0943379fdb39cc1
jayantverma11 <jayant.verma@nenosystems.in>
2025-06-30 22:57:21 +0530

    fix typo file

--- terraform-Infra/ec2.tf
+++ terraform-Infra/ec2.tf
@@ -1,5 +1,5 @@
 data "template_file" "user_data" {
-  template = file("${path.module}/user_data.sh")
+  template = file("${path.module}/user-data.sh")
   vars = {
     GH_TOKEN    = var.GH_TOKEN
     GH_USERNAME = var.GH_USERNAME
